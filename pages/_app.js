fc11b4b4aaad32a6053f414583bfba68752d259f
Ankit Tiwari <ankitt255@gmail.com>
2020-10-08 23:05:38 +0530

    📝 Limit height and width of the app on larger screen

--- pages/_app.js
+++ pages/_app.js
@@ -83,7 +83,7 @@ const Footer = () => (
 function MyApp({ Component, pageProps }) {
   return (
     <div className="flex justify-center bg-gray-200  min-h-screen max-h-screen sm:py-2">
-      <div className="flex flex-col min-h-full max-h-screen bg-pearl max-w-full sm:max-w-sm">
+      <div className="flex flex-col min-h-full max-h-screen bg-pearl max-w-full sm:max-w-sm sm:min-w-sm lg:max-h-screen-80">
         <Header title={Component.title} />
         <div className="flex-1 overflow-y-scroll">
           <Component {...pageProps} />
